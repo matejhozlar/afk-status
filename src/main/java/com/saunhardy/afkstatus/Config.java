343e9759e7085543fce3b36662aa28ddd1f6bf1d
Matej <hozlarmatej0@gmail.com>
2025-09-23 14:46:23 +0200

    1.0.0: Initial version

--- src/main/java/com/saunhardy/afkstatus/Config.java
+++ src/main/java/com/saunhardy/afkstatus/Config.java
@@ -1,64 +1,33 @@
-package com.example.examplemod;
+package com.saunhardy.afkstatus;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+public class Config {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
-@Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config
-{
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec.IntValue AFK_TRIGGER_TIMER = BUILDER
+            .comment("Time in minutes before a player is considered AFK")
+            .defineInRange("afkTriggerTimer", 5, 1, 60);
 
-    private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    public static final ForgeConfigSpec.IntValue AFK_KICK_TIMER = BUILDER
+            .comment("Time in minutes after a player is marked AFK before they are kicked (0 to disable)")
+            .defineInRange("afkKickTimer", 0, 0, 120);
 
-    private static final ForgeConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.ConfigValue<String> KICK_MESSAGE = BUILDER
+            .comment("Kick message shown to players kicked for being AFK")
+            .define("kickMessage", "You were kicked for being AFK too long.");
 
-    public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
+    public static final ForgeConfigSpec.BooleanValue SYSTEM_MESSAGES = BUILDER
+            .comment("System messages (Player is now AFK., ...)")
+            .define("systemMessages", true);
 
-    // a list of strings that are treated as resource locations for items
-    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+    public static final ForgeConfigSpec.IntValue CHECK_INTERVAL_TICKS = BUILDER
+            .comment("How often (in ticks) to check for AFK players (20 ticks = 1 second)")
+            .defineInRange("checkIntervalTicks", 20, 1, 1200);
 
-    static final ForgeConfigSpec SPEC = BUILDER.build();
+    public static final ForgeConfigSpec.ConfigValue<String> MESSAGE_COLOR = BUILDER
+            .comment("Color for AFK messages. Valid options: black, dark_blue, dark_green, dark_aqua, dark_red, dark_purple, gold, gray, dark_gray, blue, green, aqua, red, light_purple, yellow, white")
+            .define("messageColor", "yellow");
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
-    public static Set<Item> items;
-
-    private static boolean validateItemName(final Object obj)
-    {
-        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(ResourceLocation.tryParse(itemName));
-    }
-
-    @SubscribeEvent
-    static void onLoad(final ModConfigEvent event)
-    {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(ResourceLocation.tryParse(itemName)))
-                .collect(Collectors.toSet());
-    }
+    public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
