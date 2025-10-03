935b86327b1fb22929bcfda6c0e64c9aacd6cf0e
Matej <hozlarmatej0@gmail.com>
2025-10-03 23:53:42 +0200

    1.0.1: Fixed afk status remaining on server stop

--- src/main/java/com/saunhardy/afkstatus/AFKStatus.java
+++ src/main/java/com/saunhardy/afkstatus/AFKStatus.java
@@ -73,6 +73,17 @@ public class AFKStatus {
     }
 
     @SubscribeEvent
+    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            UUID uuid = player.getUUID();
+            lastPositions.remove(uuid);
+            AFKManager.setAFK(uuid, false);
+            AFKManager.removePlayerData(uuid);
+            applyAFKTag(player, false);
+        }
+    }
+
+    @SubscribeEvent
     public void onRegister(final RegisterCommandsEvent ev) {
         AFKCommand.register(ev.getDispatcher());
     }
